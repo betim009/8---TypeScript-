d2a96e625f54488d0a0d72c608ba3f06588c15c1
Alberto <betim009@gmail.com>
2022-12-21 14:50:31 -0300

    primeiros exemplos do bloco 8.1

---
+++ 8.1-Examples/dist/exercises.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSquareArea = void 0;
+function getSquareArea(side) {
+    return side ** 2;
+}
+exports.getSquareArea = getSquareArea;
