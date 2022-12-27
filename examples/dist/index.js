2ba53275797f7599a72452ca5aa2939ddb2061cd
Alberto <betim009@gmail.com>
2022-12-27 16:31:18 -0300

    classes

--- examples/dist/index.js
+++ examples/dist/index.js
@@ -19,16 +19,29 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const TA = __importStar(require("./TypeAliases"));
-console.log('Saída');
+const Classes = __importStar(require("./Classes"));
+console.log('### Saídas: ###');
 // // Array
 // console.log(Arrays.names)
 // // Tuplas
 // console.log(Arrays.anime);
 // console.log(Arrays.fullName);
 // console.log(Arrays.person);
-console.log(TA.printCoord({ x: 100, y: 150 }));
-console.log(TA.printBird({ beaks: 1, wings: 2, biped: true }));
-console.log('###');
-console.log(TA.printSum({ x: 2, y: 2 }));
-console.log(TA.printCPF(14982654727));
+// Type Aliases
+// console.log(TA.printCoord({x: 100, y:150}));
+// console.log(TA.printBird({beaks: 1, wings: 2, biped: true }));
+// console.log('###');
+// console.log(TA.printSum({x: 2, y:2}))
+// console.log(TA.printCPF(14982654727))
+// Classes
+// console.log("Classes:")
+// const person1 = new Classes.Person("Alberto Couto", new Date("1995-09-05"), 27)
+// const person2 = new Classes.Person("Creusa Fernandes", new Date("1960-08-02"), 62)
+// console.log(person1, person2);
+// person1.speak();
+// person2.walk();
+const human1 = new Classes.Human("Alberto F. Couto", new Date("1995-09-05"));
+const human2 = new Classes.Human("Creusa F. Couto", new Date("1960/08/02"), 62);
+console.log(human1, human2);
+human1.age = 27;
+console.log(human1);
